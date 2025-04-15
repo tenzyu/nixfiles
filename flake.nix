c75f45f1b91220b2e916806ab6374cb08a318b70
tenzyu <tenzyu.on@gmail.com>
2025-04-15 21:01:53 +0900

    chore: update flake.lock and flake.nix for dependency revisions and cleanup

--- flake.nix
+++ flake.nix
@@ -101,23 +101,6 @@
     ### }}}
 
     ### {{{
-    # NOTE: This will require your git SSH access to the repo.
-    #
-    # WARNING:
-    # Do NOT pin the `nixpkgs` input, as that will
-    # declare the cache useless. If you do, you will have
-    # to compile LLVM, Zig and Ghostty itself on your machine,
-    # which will take a very very long time.
-    #
-    # Additionally, if you use NixOS, be sure to **NOT**
-    # run `nixos-rebuild` as root! Root has a different Git config
-    # that will ignore any SSH keys configured for the current user,
-    # denying access to the repository.
-    #
-    # Instead, either run `nix flake update` or `nixos-rebuild build`
-    # as the current user, and then run `sudo nixos-rebuild switch`.
-    ghostty.url = "git+ssh://git@github.com/ghostty-org/ghostty";
-
     # NOTE: make sure to enable cachix first
     hyprland.url = "github:hyprwm/Hyprland";
 
