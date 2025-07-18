bb8e28ecde836cc3a1afb2fec2fb0ec49f0035fb
tenzyu <tenzyu.on@gmail.com>
2025-07-18 17:28:36 +0900

    chore: upgrade to 25.05

--- profiles/server.nix
+++ profiles/server.nix
@@ -86,7 +86,7 @@ in
       packages = with pkgs; [
         fira-code
         fira-code-symbols
-        fira-code-nerdfont
+        nerd-fonts.fira-code
         font-awesome
         noto-fonts
         noto-fonts-cjk-sans
--- user/programs/firefox/default.nix
+++ user/programs/firefox/default.nix
@@ -1,5 +1,6 @@
 {
   programs.firefox = {
     enable = true;
+    profiles.default.extensions.force = true;
   };
 }
