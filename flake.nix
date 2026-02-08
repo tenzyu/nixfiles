0dbdc581802ed38b2cbe6ee87ce7506deda81ee2
tenzyu <tenzyu.on@gmail.com>
2026-02-09 08:24:25 +0900

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -79,6 +79,9 @@
     };
 
     hyprland.url = "github:hyprwm/Hyprland";
-    catppuccin.url = "github:catppuccin/nix";
+    catppuccin = {
+      url = "github:catppuccin/nix/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
