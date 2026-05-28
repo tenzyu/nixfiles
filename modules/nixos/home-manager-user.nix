41f997b7c2f932477bffbe9a47f45ee6ffacac86
tenzyu <tenzyu.on@gmail.com>
2026-05-28 12:58:24 +0900

    chore: flake update

--- modules/nixos/home-manager-user.nix
+++ modules/nixos/home-manager-user.nix
@@ -2,15 +2,22 @@
   config,
   lib,
   ...
-}: {
-  flake.modules.nixos.homeManagerUser = {
-    home-manager.users.${config.me.username} = {
+}: let
+  inherit (config.me) stateVersion username;
+in {
+  flake.modules.nixos.homeManagerUser = {config, ...}: {
+    home-manager.users.${username} = {
       programs.home-manager.enable = lib.mkDefault true;
       xdg.enable = lib.mkDefault true;
       home.preferXdgDirectories = lib.mkDefault true;
-      home.username = lib.mkDefault config.me.username;
-      home.homeDirectory = lib.mkDefault "/home/${config.me.username}";
-      home.stateVersion = config.me.stateVersion;
+      home.username = lib.mkDefault username;
+      home.homeDirectory = lib.mkDefault "/home/${username}";
+      home.stateVersion = stateVersion;
     };
+
+    # Keep this convenience link moving with Home Manager's active generation.
+    systemd.tmpfiles.rules = [
+      "L+ /tmp/${config.networking.hostName}-home-files - - - - /home/${username}/.local/state/home-manager/gcroots/current-home/home-files"
+    ];
   };
 }
